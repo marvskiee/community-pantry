ce9b350786f5733457233bfd1e3fa549286e8f90
unknown <acemonkey2@gmail.com>
2022-09-08 13:18:31 +0800

    fix style

--- pages/index.js
+++ pages/index.js
@@ -7,10 +7,10 @@ const Home = () => {
       <NavBar />
       <div className="bg-slate-900 w-full relative min-h-screen-nav">
         <div className="relative p-10 flex flex-col gap-5 items-center justify-center z-10 min-h-screen-nav bg-slate-900/50">
-          <p className="text-6xl text-white font-bold p-10 text-center">
+          <p className="sm:text-6xl text-3xl text-white font-bold text-center">
             Barangay Community Pantry
           </p>
-          <button className="p-4 text-2xl text-white bg-emerald-500 rounded-full font-bold px-10 ">
+          <button className="p-4 sm:text-2xl text-xl text-white bg-emerald-500 rounded-full font-bold px-10 ">
             Get Started with us
           </button>
         </div>
